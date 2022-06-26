681642f4346fedc0dd7f66c3b6035d0c1dd0317a
marienoir <okosunmaryeghonghon@gmail.com>
2022-06-27 00:14:25 +0100

    Implemented repository feature

--- src/main/com/dufuna/berlin/maryOkosun/lekki/MockLekkiPropertyApp.java
+++ src/main/com/dufuna/berlin/maryOkosun/lekki/MockLekkiPropertyApp.java
@@ -1,26 +1,38 @@
 package com.dufuna.berlin.maryOkosun.lekki;
 import com.dufuna.berlin.maryOkosun.lekki.model.LekkiProperty;
-import com.dufuna.berlin.maryOkosun.lekki.model.MockLekkiPropertyService;
+import com.dufuna.berlin.maryOkosun.lekki.repository.SimpleLekkiPropertyRepository;
+import com.dufuna.berlin.maryOkosun.lekki.repository.SimpleLekkiPropertyRepositoryImpl;
+
+import java.util.Date;
+
+import static com.dufuna.berlin.maryOkosun.lekki.repository.SimpleLekkiPropertyRepository.trees;
 
 public class MockLekkiPropertyApp {
     public static void main(String[] args) {
-        LekkiProperty property1 = new LekkiProperty();
-        property1.setPropertyId(1);
-        property1.setPropertyAddress("12,Chance Road,New York");
-        property1.setPropertyType("Story Building");
-        property1.setDescription("This is a beautiful apartment");
-        property1.setNumberOfBathrooms(3);
-        property1.setNumberOfBedrooms(5);
-        property1.setPropertyOwner("Mary Okosun");
-        property1.setNumberOfKitchens(1);
-        property1.setNumberOfSittingRooms(5);
-        property1.setNumberOfToilets(3);
-        property1.setValidFrom("12/10/2022");
-        property1.setValidTo("29/12/2023");
-        System.out.println("Property created successfully");
+        String[] typeArr = new String[]{ "Bungalow","Duplex","Storey Building" };
+        String[] addressArr = new String[]{ "Lagos","Canada","New York" };
+        for (int i = 0; i < typeArr.length; i = i + 1) {
+            LekkiProperty property1 = new LekkiProperty(i+1, typeArr[i], addressArr[i]);
+            property1.setDescription("This is a beautiful apartment");
+            property1.setNumberOfBathrooms(3);
+            property1.setNumberOfBedrooms(5);
+            property1.setPropertyOwner("Mary Okosun");
+            property1.setNumberOfKitchens(1);
+            property1.setNumberOfSittingRooms(5);
+            property1.setNumberOfToilets(3);
+            property1.setValidFrom(new Date(2022, 11, 21));
+            property1.setValidTo(new Date(2023, 12, 11));
+            System.out.println("Property created successfully : " + property1);
+            trees.put(i+1, property1);
 
-        MockLekkiPropertyService myProperty = new MockLekkiPropertyService();
-        myProperty.saveProperty(property1);
-        myProperty.getProperty();
+        }
+        LekkiProperty property = new LekkiProperty(1,"Bungalow", "12 Chance Road, New York");
+        LekkiProperty updatedProperty = new LekkiProperty(1,"Storey Building", "Canada");
+
+        SimpleLekkiPropertyRepository propertyRepository = new SimpleLekkiPropertyRepositoryImpl();
+        propertyRepository.save(trees);
+        propertyRepository.findById(3, property);
+        propertyRepository.findAll(trees);
+        propertyRepository.update(1,updatedProperty);
     }
 }
--- src/main/com/dufuna/berlin/maryOkosun/lekki/model/LekkiPropertyService.java
+++ src/main/com/dufuna/berlin/maryOkosun/lekki/model/LekkiPropertyService.java
@@ -1,8 +1,53 @@
 package com.dufuna.berlin.maryOkosun.lekki.model;
 
-public interface LekkiPropertyService {
-    public String saveProperty(String LekkiProperty);
-    public LekkiProperty getProperty();
+import com.dufuna.berlin.maryOkosun.lekki.repository.SimpleLekkiPropertyRepository;
+import com.dufuna.berlin.maryOkosun.lekki.repository.SimpleLekkiPropertyRepositoryImpl;
+
+import java.util.HashMap;
+
+interface LekkiPropertyService {
+    public void saveProperty(LekkiProperty LekkiProperty);
+    public void getProperty();
+
+    class LekkiPropertyRepository {
+        LekkiProperty saveProperty(LekkiProperty lekkiProperty) {
+            SimpleLekkiPropertyRepository saveProp = new SimpleLekkiPropertyRepository() {
+                @Override
+                public HashMap<Integer, LekkiProperty> save(HashMap<Integer, LekkiProperty> trees) {
+                    return null;
+                }
+
+                @Override
+                public LekkiProperty findById(int id, LekkiProperty lekkiProperty) {
+                    return null;
+                }
+
+                @Override
+                public HashMap<Integer, LekkiProperty> findAll(HashMap<Integer, LekkiProperty> trees)  {
+                    return null;
+                }
+
+                @Override
+                public LekkiProperty update(int id, LekkiProperty lekkiProperty) {
+                    return lekkiProperty;
+                }
+            };
+            return lekkiProperty;
+        };
+    }
+    static void main(String[] args) {
+        LekkiProperty property = new LekkiProperty(1,"Bungalow", "Ondo State");
+        LekkiProperty property1 = new LekkiProperty(1,"Bungalow", "Lagos State");
+
+        SimpleLekkiPropertyRepository propertyRepository = new SimpleLekkiPropertyRepositoryImpl();
+//        propertyRepository.save(property);
+        propertyRepository.findById(property.getPropertyId(), property);
+//        propertyRepository.findAll(property);
+        propertyRepository.update(1,property1);
+    }
 }
+
+
+
 
 
